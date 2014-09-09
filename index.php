da9b5dc824957f5c66ca5c8af877c22bfb3bf644
peregraum <peregm98@gmail.com>
2014-09-09 02:28:23 +0200

    Base del header y estilos para la página principal

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <html>
 <head>
     <link rel="stylesheet" href="resources/css/main.css" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Bubblegum+Sans|Fugaz+One|Jockey+One|Jura:400,600|Orbitron:400,900' rel='stylesheet' type='text/css'>
+    <meta charset="utf-8">
 </head>
 <body>
-
+    <header>
+        <a href="index.php" id="logo">
+            <h1>MataJuegos</h1>
+            <h2>Con una aerodinámica a tu gusto!</h2>
+        </a>
+        <nav>
+            <ul id="menu">
+                <li>Home</li>
+                <li>Notícias</li>
+                <li>Categorías</li>
+                <li>Top</li>
+            </ul>
+            <div id="search"><input type="text" placeholder="Buscar"></div>
+            <ul id="login">
+                <li>Login</li>
+                <li>Regístrate</li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
