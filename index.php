3c971c132c63008f4d9232b799eb77b2fa5a8add
peregraum <peregm98@gmail.com>
2014-09-09 23:34:00 +0200

    Puesto el buscador en su sitio.

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             <li>Categorías</li>
             <li>Top</li>
         </ul>
-<!--        <input type="text" placeholder="Buscar" id="search">-->
+        <input type="text" placeholder="Buscar" id="search">
         <ul id="login">
             <li>Login</li>
             <li>Regístrate</li>
@@ -30,5 +30,7 @@
 DimEh sEh coNmiGoH' kieReh asEr träVesUrAss
 
 <img src="http://3.bp.blogspot.com/_O8OCaC3a-iI/TFltTLDztlI/AAAAAAAABE4/a7e4rmgB3iI/s400/rata_curiosoanimal.blogspot.com+(4).jpg">
+<br><br>
+<h1>VALE WILLY! SFSFSF tt</h1>
 </body>
 </html>
