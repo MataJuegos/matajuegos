c9dc505e96fa7c1d9acfec407d721a652290533c
peregraum <peregm98@gmail.com>
2014-09-09 23:28:35 +0200

    Arreglada la basura de la barra de nav y el 50%.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <head>
     <title>Matajuegos - Con una aerodinámica a tu gusto!</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="resources/css/normalize.css" type="text/css">
     <link rel="stylesheet" href="resources/css/main.css" type="text/css">
     <link href='http://fonts.googleapis.com/css?family=Bubblegum+Sans|Fugaz+One|Jockey+One|Jura:400,600|Orbitron:400,900' rel='stylesheet' type='text/css'>
 </head>
@@ -18,12 +19,16 @@
             <li>Categorías</li>
             <li>Top</li>
         </ul>
-        <input type="text" placeholder="Buscar" id="search">
+<!--        <input type="text" placeholder="Buscar" id="search">-->
         <ul id="login">
             <li>Login</li>
             <li>Regístrate</li>
         </ul>
     </nav>
 </header>
+<br><br>
+DimEh sEh coNmiGoH' kieReh asEr träVesUrAss
+
+<img src="http://3.bp.blogspot.com/_O8OCaC3a-iI/TFltTLDztlI/AAAAAAAABE4/a7e4rmgB3iI/s400/rata_curiosoanimal.blogspot.com+(4).jpg">
 </body>
 </html>
